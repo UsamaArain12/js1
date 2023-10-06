24640afab2baab258b05cec9748956995337a294
Usama Arain <ua339716@gmail.com>
2023-10-05 22:01:53 -0700

    js1

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("Error! Please enter a valid Password");
+alert("Welcome to JS Land... \nHappy coding!");
+alert("Welcome to JS Land...");
+alert("Happy coding! \nPrevent this page from creating additionals dialogs");
+alert("Hello...! I can run JS through my web browser's console");
+
